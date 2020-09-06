b44b60e7e8e9cce502ed352b1436434a220b8a97
Josh Barua <joshbarua2002@gmail.com>
2020-09-06 12:12:21 -0500

    Add comments to code

--- Cosine Similarity between Vectors.py
+++ Cosine Similarity between Vectors.py
@@ -1,5 +1,5 @@
 from scipy import spatial
 
-feature_vector = [1,1,1] #this vector represents the preffered features specified by a user (e.g., cushion, comfort, support)
-review_vector = [1,0,1] #this vector represents a single review where 0 indicates the review did not contain the feature and 1 indicates that the review did contain the feature (e.g., cushion, comfort, support)
+feature_vector = [1,1,1] #this vector represents the preferred features specified by a user (e.g., cushion, comfort, support)
+review_vector = [1,0,1] #this vector represents a single review where 0 indicates that the review did not contain the feature and 1 indicates that the review did contain the feature (e.g., cushion, comfort, support)
 cosine_similarity = 1 - spatial.distance.cosine(feature_vector, review_vector) #calculates the cosine similarity between the two vectors
